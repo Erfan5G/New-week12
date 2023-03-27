5b36514549f8f3e775309fcdeb034c08bbbb8d14
Erfan Goudarzi <erfangoudarzi@gmail.com>
2023-03-27 10:31:28 -0700

    Adding click eventlistener

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,9 @@
-{} 
-{{ 
-    {{}} {
-    
-}}
+const buttonref = document.querySelector("button") ; 
+function alerter () {
+    alert("You clicked me!") 
+    // buttonref.removeEventListener("click",alerter)
+
+}
+
+buttonref.addEventListener ("click",alerter , {once:true}) 
+// buttonref.addEventListener("click")

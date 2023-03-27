03a41322ab3c86e62c893d6678622ae86c71e666
Erfan Goudarzi <erfangoudarzi@gmail.com>
2023-03-27 10:58:32 -0700

    Adding image in Js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,11 +18,30 @@ function clicked() {
     else {
         buttonref.textContent = "Click Me !"
     }   
+}
 
-    
+function updatedimage() {
+    const image = document.querySelector("img")
+    // console.log(image)
+    image.setAttribute (
+        "src" , 
+        "iamges/Shopping-Cart.png"
+    )
+    image.setAttribute(
+        "alt",
+        "SHopping cart image"
+    )
+    image.setAttribute(
+        "width" , 
+        "50" 
+    )
+    image.setAttribute(
+        "height" ,
+        "50" 
+    )
 }
 // buttonref.addEventListener ("click",alerter , {once:true}) 
 // buttonref.addEventListener("click")
 buttonref.addEventListener('click' , clicked)
 buttonref.addEventListener('click' , backgroundclicked)
-
+updatedimage() 
